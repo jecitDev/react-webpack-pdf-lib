fda2b240d11eeb1c06ae955dd66edeb738eb416b
fernandojec <fernando.riyo@jec.co.id>
2024-10-04 16:34:41 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PDFDocument, rgb } from "pdf-lib";
+
+const App = () => <h1>Hello, React with Webpack 5!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
