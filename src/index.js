f51fffd6e8324dab14d6495f08ed727e7a7813b0
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-10-05 17:45:41 +0700

    pdf worker

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 // Set up the worker for react-pdf
-// pdfjs.GlobalWorkerOptions.workerSrc = pdfjs.GlobalWorkerOptions.workerSrc =
-//   new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   `pdfjs-dist/build/pdf.worker.min.js`,
+//   import.meta.url
+// ).toString();
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const App = () => {
